ccc3eb3dc72e0fc5c4e09aba016ccc9e76491d46
akagaeng <akagaeng@gmail.com>
2018-02-13 16:04:54 +0900

    add route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var cv = require('opencv4nodejs');
+if (cv) {
+    console.log('opencv4nodejs successfully installed!');
+}
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+/* GET dummy page. */
+router.get('/', (req, res) => {
+    res.render('index', { title: 'Opencv4nodejs dashboard' });
 });
 
+router.post('/faces/detection', (req, res) => {
+    res.send('(res) POST /faces/detection');
+});
+
+router.post('/faces/recognition', (req, res) => {
+    res.send('(res) POST /faces/recognition');
+});
 module.exports = router;
